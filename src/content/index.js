a045899412f51f8ce42bfe05612d562c1ca863e2
zhangpan <zhangpanrobot@gmail.com>
2023-07-03 02:25:47 +0800

    rule

--- src/content/index.js
+++ src/content/index.js
@@ -8,3 +8,33 @@ const hiddenEl = createEle({
 document.body.appendChild(hiddenEl)
 
 setTimeout(() => list('.hidden-el'), 3000)
+
+const rules = {
+  zhihu: {
+    selector: '.MathJax_SVG',
+    parser: (el) => {
+      console.log(el.getAttribute('data-mathml'))
+    },
+  },
+}
+
+function createCopyIcon(target) {
+  const span = document.createElement('span')
+  span.textContent = 'copy'
+  span.className = 'sss-copy'
+  target.appendChild(span)
+}
+
+document.addEventListener('mouseover', (e) => {
+  const target = e.target
+  if (target.className.contains(rules.zhihu.selector)) {
+    createCopyIcon(target)
+  }
+})
+
+document.addEventListener('click', (e) => {
+  const target = e.target
+  if (target.className.contains('sss-copy')) {
+    rules.zhihu.parser(target)
+  }
+})
