51fbaedafcfcd5263a5364d124b8cf59da7a7c7c
wudaxue <1203588983@qq.com>
2023-08-07 01:45:37 +0800

    format code

--- src/content/const.js
+++
@@ -1,3 +0,0 @@
-export const selectorList = ['.MathJax_Preview + .MathJax_SVG_Display', '.MathJax_Preview + .MathJax_SVG', '.MathJax']
-
-export const juejinSelectorList = ['.katex-mathml annotation']
