b993e3414f8f37fe86f4c8f027003abc89c71585
wudaxue <1203588983@qq.com>
2023-09-02 20:14:04 +0800

    build zip

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { crx } from '@crxjs/vite-plugin'
 import eslintPlugin from 'vite-plugin-eslint'
 import tailwind from 'tailwindcss'
 import manifest from './manifest.json'
+import zipPack from 'vite-plugin-zip-pack'
 
 export default defineConfig({
   resolve: {
@@ -14,7 +15,7 @@ export default defineConfig({
   output: {
     sourcemap: 'inline',
   },
-  plugins: [crx({ manifest }), eslintPlugin()],
+  plugins: [crx({ manifest }), eslintPlugin(), zipPack({ outDir: './' })],
   css: {
     // https://github.com/vitejs/vite/discussions/8216
     modules: {
